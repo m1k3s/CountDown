85ec5c077eed56e85138aad0fb14bf788a4a45c3
m1k3s <pbrane0@gmail.com>
2016-11-27 16:43:18 -0500

    added persistence, the end date is now saved for future sessions.

--- app/src/main/java/com/pbrane/mike/countdown/MainActivity.java
+++ app/src/main/java/com/pbrane/mike/countdown/MainActivity.java
@@ -1,11 +1,12 @@
 package com.pbrane.mike.countdown;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 	public TextView textView;
 	String retirementDateTime = "01/01/2023 17:00:00"; // dd/M/yyyy hh:mm:ss
+	private DatePicker dp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 		textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36.0f);
 		textView.setTextColor(Color.GRAY);
 
-		final DatePicker dp = (DatePicker)findViewById(R.id.datePicker);
+		dp = (DatePicker)findViewById(R.id.datePicker);
 		dp.init(2023, 0, 1, null); // month is zero-offset
 
 		Button button = (Button)findViewById(R.id.button);
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 				int month = dp.getMonth() + 1; // month is zero-offset
 				int day = dp.getDayOfMonth();
 				retirementDateTime = String.format(Locale.getDefault(), "%02d/%02d/%4d 17:00:00", day, month, year);
+				saveDateTime(String.format(Locale.getDefault(), "%04d%02d%02d170000", year, month, day));
 			}
 		});
 
@@ -66,11 +69,74 @@ public class MainActivity extends AppCompatActivity {
 					e.printStackTrace();
 				}
 			}
-		}, 1000);
+		}, 250);
+
+		if (savedInstanceState != null) {
+			String date = savedInstanceState.getString("DateTime");
+			int[] dateTime = parseDateTimeString(date);
+			if (dateTime != null) {
+				dp.init(dateTime[0], dateTime[1] - 1, dateTime[2], null);
+				retirementDateTime = String.format(Locale.getDefault(), "%02d/%02d/%4d %02d:%02d:%02d", dateTime[2], dateTime[1], dateTime[0], dateTime[3], dateTime[4], dateTime[5]);
+			}
+		} else {
+			SharedPreferences sharedPref = this.getPreferences (MODE_PRIVATE);
+			String date = sharedPref.getString(getString(R.string.savedDateTime), "");
+			int[] dateTime = parseDateTimeString(date);
+			if (dateTime != null) {
+				dp.init(dateTime[0], dateTime[1] - 1, dateTime[2], null);
+				retirementDateTime = String.format(Locale.getDefault(), "%02d/%02d/%4d %02d:%02d:%02d", dateTime[2], dateTime[1], dateTime[0], dateTime[3], dateTime[4], dateTime[5]);
+			}
+		}
 
 	}
 
+	@Override
+	public void onSaveInstanceState (@NonNull Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+
+		String dateTime = String.format(Locale.getDefault(), "%04d%02d%02d", dp.getYear(), dp.getMonth() + 1, dp.getDayOfMonth());
+		dateTime += "170000";
+		System.out.println("*** DatePicker.toString()" + dateTime);
+		savedInstanceState.putString("DateTime", dateTime);
+	}
+
+	@Override
+	public void onRestoreInstanceState (@NonNull Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+
+		String date = savedInstanceState.getString("DateTime");
+		int[] dateTime = parseDateTimeString(date);
+		if (dateTime != null) {
+			dp.init(dateTime[0], dateTime[1] - 1, dateTime[2], null);
+			retirementDateTime = String.format(Locale.getDefault(), "%02d/%02d/%4d %02d:%02d:%02d", dateTime[2], dateTime[1], dateTime[0], dateTime[3], dateTime[4], dateTime[5]);
+		}
+	}
+
+	private void saveDateTime(String dateTime) {
+		SharedPreferences sharedPrefs = this.getPreferences (MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPrefs.edit();
+		editor.putString(getString(R.string.savedDateTime), dateTime);
+		editor.apply();
+	}
+
+	// array = year, month, day, hour, minute, second
+	private int[] parseDateTimeString(String str) {
+		if (str.length() > 0) {
+			int year = Integer.valueOf(str.substring(0, 4));
+			int month = Integer.valueOf(str.substring(4, 6));
+			int day = Integer.valueOf(str.substring(6, 8));
+			int hour = Integer.valueOf(str.substring(8, 10));
+			int minute = Integer.valueOf(str.substring(10, 12));
+			int second = Integer.valueOf(str.substring(12));
+			return new int[]{year, month, day, hour, minute, second};
+		}
+
+		return null;
+	}
+
 	private void printDifference(Date startDate, Date endDate) {
+		System.out.println("Start Date: " + startDate.toString());
+		System.out.println("End Date: " + endDate.toString());
 		Interval interval = new Interval(startDate.getTime(), endDate.getTime());
 		Period period = interval.toPeriod();
 
